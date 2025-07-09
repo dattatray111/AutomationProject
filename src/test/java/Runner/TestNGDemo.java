e635730bc930f46b8d91a588f32e52b90b119ad5
dattatray111 <dattatray111@.com>
2025-07-09 19:37:58 +0530

    Adding TestNg script

---
+++ src/test/java/Runner/TestNGDemo.java
@@ -0,0 +1,27 @@
+package Runner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+	@Test
+	public static void T1() {
+		
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.flipkart.com/");
+			
+			 JavascriptExecutor js = (JavascriptExecutor) driver;
+			 
+			 WebElement element = driver.findElement(By.className("_3ZqtNW"));
+			
+			js.executeScript("arguments[0].setAttribute('style', arguments[1]);", 
+                    element, "border: 4px solid red");
+
+	}
+
+}
